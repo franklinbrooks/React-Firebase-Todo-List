12b97f734ce627a08d7ad7e9e48291d5252ee6da
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-01-12 12:04:59 -0500

    User can edit a todo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import moment from 'moment';
 class App extends Component {
   constructor() {
     super();
-    this.state = { todos: {} };
+    this.state = {
+      todos: {},
+      edit: false
+    };
 
     this.handleNewTodoInput = this.handleNewTodoInput.bind(this);
     this.deleteTodo = this.deleteTodo.bind(this);
+    this.enableEditMode = this.enableEditMode.bind(this);
+    this.updateCurrentTodo = this.updateCurrentTodo.bind(this);
   }
 
   componentDidMount() {
@@ -110,18 +115,60 @@ class App extends Component {
     })
   }
 
+  updateCurrentTodo() {
+    let id = this.state.currentTodo;
+    let currentTodo = this.state.todos[id];
+    currentTodo.title = this.refs.editTodoInput.value;
+
+    axios({
+      url: `/todos/${id}.json`,
+      baseURL: 'https://todo-31265.firebaseio.com/',
+      method: "PATCH",
+      data: currentTodo
+    }).then((response) => {
+      let todos = this.state.todos;
+      todos[id] = currentTodo;
+      this.setState({
+        todos: todos,
+        edit: false
+      });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   renderSelectedTodo() {
     let content;
 
     if (this.state.currentTodo) {
       let currentTodo = this.state.todos[this.state.currentTodo];
-      content =  (
-        <div>
-          <h1>{currentTodo.title}</h1>
-        </div>
-      );
+      if(!this.state.edit) {
+        content =  (
+          <div>
+            <div className="d-flex justify-content-end mb-3">
+              <button onClick={this.enableEditMode}>Edit</button>
+            </div>
+            <h1>{currentTodo.title}</h1>
+          </div>
+        );
+      } else {
+        content =  (
+          <div>
+            <div className="d-flex justify-content-end mb-3">
+              <button onClick={this.updateCurrentTodo}>Save</button>
+            </div>
+            <input className="w-100" defaultValue={currentTodo.title} ref="editTodoInput" />
+          </div>
+        );
+      }
     }
     return content;
+  }
+
+  enableEditMode() {
+    this.setState({
+      edit: true
+    })
   }
 
   render() {
